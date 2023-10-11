1c309269bc56c750d8d9fc8130403cfad759e2df
jeffersontee <jeffersontee@gmail.com>
2023-10-11 01:08:02 -0300

    feat: add form control

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
   templateUrl: './forms.component.html',
   styleUrls: ['./forms.component.scss']
 })
-export class FormsComponent {
+export class FormsComponent implements OnInit {
 
   results = []
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  //ngOnInit e como se fosse UseEffet que e utilizado pelo React com o array de dependencia vazio
+  ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+  }
+
+  onSubmit(): void {
+    console.log(this.formGroup.value)
+  }
+
+  onClean(): void {
+    this.formGroup.reset()
+  }
+
+  
 
 }
