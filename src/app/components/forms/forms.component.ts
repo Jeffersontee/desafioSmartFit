d4fbdc4a4c6490f27ebd9384be800a7e978965f4
jeffersontee <jeffersontee@gmail.com>
2023-10-11 11:30:14 -0300

    feat: add filter to open units

--- src/app/components/forms/forms.component.ts
+++ src/app/components/forms/forms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { GetUnitsService } from 'src/app/services/get-units.service';
+import { Location } from 'src/app/types/location.interface';
 
 @Component({
   selector: 'app-forms',
@@ -9,22 +10,32 @@ import { GetUnitsService } from 'src/app/services/get-units.service';
 })
 export class FormsComponent implements OnInit {
 
-  results = []
+  results: Location[] = [];
+  filterResults: Location[] = [];
   formGroup!: FormGroup;
 
   constructor(private formBuilder: FormBuilder, private unitService: GetUnitsService) { }
 
   //ngOnInit e como se fosse UseEffet que e utilizado pelo React com o array de dependencia vazio
   ngOnInit(): void {
-    this.unitService.getAllUnits().subscribe(data => console.log(data));
-    this.formGroup = this.formBuilder.group({
+     this.formGroup = this.formBuilder.group({
       hour: '',
-      showClosed: false
+      showClosed: true
     })
+    this.unitService.getAllUnits().subscribe(data => {
+        this.results = data.locations;
+        this.filterResults = data.locations;
+    });
+   
   }
 
   onSubmit(): void {
     console.log(this.formGroup.value)
+    if (!this.formGroup.value.showClosed) {
+      this.filterResults = this.results.filter(location => location.opened === true )
+    } else {
+      this.filterResults = this.results;
+    }
   }
 
   onClean(): void {
